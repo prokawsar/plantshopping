5b023979a96016666dca9492e847f9b50a9c9d49
prokawsar <prokawsar@gmail.com>
2024-10-26 16:15:27 +0600

    Fix landing page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+import { useState } from "react";
+import ProductList from "./ProductList";
+import "./App.css";
+import AboutUs from "./AboutUs";
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -14,25 +12,33 @@ function App() {
 
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+      <div
+        className={`flex flex-col h-screen overflow-auto w-full${
+          showProductList ? "fade-out" : ""
+        }`}
+      >
         <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+        <div className="text-white backdrop-blur-sm h-full items-center w-full bg-black bg-opacity-20 gap-5 lg:gap-10 flex flex-col lg:flex-row p-5">
+          <div className="flex flex-col w-1/4 text-center items-center">
+            <h1 className="text-5xl">Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p className="text-2xl">Where Green Meets Serenity</p>
 
+            <button
+              className="py-4 px-6 bg-green-600 rounded-md flex items-center"
+              onClick={handleGetStartedClick}
+            >
+              Get Started
+            </button>
+          </div>
+          <div className="text-2xl w-3/4 p-10">
+            <AboutUs />
+          </div>
+        </div>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+      <div
+        className={`product-list-container ${showProductList ? "visible" : ""}`}
+      >
         <ProductList />
       </div>
     </div>
@@ -40,6 +46,3 @@ function App() {
 }
 
 export default App;
-
-
-
